7b69e96e2b861d652c70367c98d63cddaf04d810
金正朋也 <mhtnim1023@gmail.com>
2019-05-25 10:43:13 +0900

    sql作成

---
+++ db/sql/create_muscle_table.sql
@@ -0,0 +1,22 @@
+CREATE TABLE IF NOT EXISTS animals
+(
+        id MEDIUMINT NOT NULL
+        AUTO_INCREMENT,
+     name CHAR
+        (30) NOT NULL,
+     PRIMARY KEY
+        (id)
+);
+
+INSERT INTO animals
+        (name)
+VALUES
+        ('dog'),
+        ('cat'),
+        ('penguin'),
+        ('lax'),
+        ('whale'),
+        ('ostrich');
+
+SELECT *
+FROM animals;
